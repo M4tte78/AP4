87c6da3be387471e029f943146e848857978cce0
M4tte78 <baillymatteo@icloud.com>
2024-01-19 09:58:45 +0100

    Ajout de la navbar widget (navbar du bas) sur toutes les pages

---
+++ lib/profil.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'navbarwidget.dart';
+
+class ProfilPage extends StatelessWidget {
+  const ProfilPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profil'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              onTap: () {
+                print('Changer la photo de profil');
+              },
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/astronaut.png'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Nom d\'utilisateur'),
+              trailing: const Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: const Icon(Icons.mail),
+              title: const Text('Email'),
+              trailing: const Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: const Icon(Icons.lock),
+              title: const Text('Mot de passe'),
+              trailing: const Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Préférences'),
+              trailing: const Icon(Icons.edit),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: NavBarWidget(),  // Placez le NavBarWidget ici
+    );
+  }
+}
